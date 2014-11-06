a9135eeabe8928a11a9513ff69e7cf51a3dc63b6
VilsonLu <vilson_lu@yahoo.com>
2014-11-07 01:10:37 +0800

    - added register page

--- src/com/example/pandapanic/MenuActivity.java
+++ src/com/example/pandapanic/MenuActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MenuActivity extends Activity{
 
--- src/model/Account.java
+++ src/model/Account.java
@@ -10,6 +10,7 @@ public class Account implements Serializable{
 	private String id;
 	private String username;
 	private String password;
+	private String email;
 	
 	public String getId() {
 		return id;
@@ -29,9 +30,14 @@ public class Account implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
-	
-	 @Override
+	@Override
     public String toString() {
         return "Account [id=" + id + ", username=" + username + "]";
     }   
